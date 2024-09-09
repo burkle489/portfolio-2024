931a4a2749d30f77486515c41cebe50f8222ea21
taylerburkefluid <tayler.burke@fluidesign.co.uk>
2024-09-09 22:39:21 +0200

    mobile responsiveness for home page

--- src/components/UI/Blades/HomeHero/index.tsx
+++ src/components/UI/Blades/HomeHero/index.tsx
@@ -10,17 +10,17 @@ const HomeHero: FC<IHomeHeroProps> = ({ className }) => {
   return (
     <section
       className={clsx(
-        "basic-hero z-10 w-full flex flex-col justify-center h-[calc(100vh)] section relative ",
+        "basic-hero z-10 w-full flex flex-col justify-center h-[calc(100vh)] section relative",
         className
       )}
     >
       <div className="w-11/12 h-fit mx-auto">
         <div className="relative w-full flex flex-col justify-center">
-          <div className="w-full h-full relative grid grid-cols-2">
-            <div className="col-span-2 flex flex-col w-full items-start justify-start ">
+          <div className="w-full h-full relative grid grid-cols-2 gap-10">
+            <div className="col-span-2 flex flex-col w-full items-start justify-start">
               <Heading
                 variant="h1"
-                className="max-w-screen-md font-bold text-border !text-8xl uppercase z-20 !mb-0 text-light text-start !leading-snug"
+                className="max-w-screen-md font-bold text-border !text-6xl md:!text-8xl uppercase z-20 md:!mb-0 text-light text-start !leading-[0.9] md:!leading-snug"
                 // animationInDirection="left"
                 animationDelay={0}
                 scrubTrigger="basic-hero"
@@ -30,7 +30,7 @@ const HomeHero: FC<IHomeHeroProps> = ({ className }) => {
               </Heading>
               <Heading
                 variant="h2"
-                className="!font-playfair max-w-screen-md italic !mb-8 text-border !text-5xl z-20 !font-semibold text-light "
+                className="!font-playfair max-w-screen-md italic !mb-8 text-border !text-3xl md:!text-5xl z-20 !font-semibold text-light !leading-[1.2] "
                 animationInDirection="left"
                 animationDelay={1}
                 scrubTrigger="basic-hero"
@@ -38,14 +38,14 @@ const HomeHero: FC<IHomeHeroProps> = ({ className }) => {
               >
                 Full-Stack Developer
               </Heading>
-              <Paragraph className="max-w-screen-md !text-2xl">
+              <Paragraph className="max-w-screen-md md:!text-2xl">
                 Nice to meet you!
                 <br /> I&apos;m a passionate and experienced full-stack web
                 developer with a knack for crafting digital solutions that
                 exceed expectations.
               </Paragraph>
             </div>
-            <div className="text-light col-span-2 flex flex-col justify-end items-end w-full">
+            <div className="text-light col-span-2 flex flex-col  justify-end items-center md:items-end w-full">
               <HomeHeroNav />
             </div>
           </div>
--- src/components/UI/Organisms/HomeHeroNav/index.tsx
+++ src/components/UI/Organisms/HomeHeroNav/index.tsx
@@ -7,27 +7,27 @@ const HomeHeroNav: FC<IClassName> = ({ className }) => {
   return (
     <div
       className={clsx(
-        "text-light flex flex-col justify-center items-start h-full col-span-1",
+        "text-light flex flex-col justify-center items-center md:items-start h-full col-span-1",
         className
       )}
     >
       <AnchorLink
         href={"/work"}
-        className="text-9xl uppercase font-bold !font-playfair italic text-border"
+        className="text-6xl md:text-9xl uppercase font-bold !font-playfair italic text-border"
         animationDelay={2}
       >
         Work
       </AnchorLink>
       <AnchorLink
         href={"/about"}
-        className="text-9xl uppercase font-bold !font-playfair italic text-border"
+        className="text-6xl md:text-9xl uppercase font-bold !font-playfair italic text-border"
         animationDelay={2.3}
       >
         About
       </AnchorLink>
       <AnchorLink
         href={"/contact"}
-        className="text-9xl uppercase font-bold !font-playfair italic text-border"
+        className="text-6xl md:text-9xl uppercase font-bold !font-playfair italic text-border"
         animationDelay={2.6}
       >
         Contact
